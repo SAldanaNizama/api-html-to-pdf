f73a21aa97945aeedfaaefa99ee5127019fe94a4
SAldanaNizama <140188751+SAldanaNizama@users.noreply.github.com>
2024-10-10 16:33:53 -0500

    fix-pdfservicer

--- src/services/pdfService.js
+++ src/services/pdfService.js
@@ -1,27 +1,31 @@
 const puppeteer = require("puppeteer");
-const chromium = require("chrome-aws-lambda"); // Para entornos como Railway
 
-// Función para generar el PDF
 exports.generatePDF = async (html) => {
+  let browser;
   try {
-    // Lanza el navegador
-    const browser = await puppeteer.launch({
-      args: chromium.args,
-      executablePath: await chromium.executablePath,
-      headless: true, // Asegúrate de que esté en true para producción
+    browser = await puppeteer.launch({
+      headless: true,
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage",
+      ],
+      // La siguiente línea es opcional y se puede incluir si hay problemas de tiempo de espera
+      timeout: 30000, // Aumenta el tiempo de espera
     });
 
     const page = await browser.newPage();
-    await page.setContent(html);
 
-    // Genera el PDF
+    await page.setContent(html, { waitUntil: "domcontentloaded" });
     const pdfBuffer = await page.pdf({ format: "A4" });
 
-    await browser.close(); // Cierra el navegador
-
-    return pdfBuffer; // Retorna el buffer del PDF
+    return pdfBuffer;
   } catch (error) {
-    console.error("Error generating PDF with Puppeteer:", error);
-    throw error; // Lanza el error para manejarlo en el controlador
+    console.error("Error in PDF generation:", error);
+    throw error;
+  } finally {
+    if (browser) {
+      await browser.close();
+    }
   }
 };
